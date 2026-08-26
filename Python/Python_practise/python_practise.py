dc0c01c4eec6539892a946a72dce3a2e154e3395
kushagra20042 <sharmakushagra058@gmail.com>
2026-08-27 01:40:08 +0530

    practise python

--- Python/Python_practise/python_practise.py
+++ Python/Python_practise/python_practise.py
@@ -1,1 +1,61 @@
-print("hello world")
+"solution 1"'''a=int(input())
+if a>0:
+    print("positive")
+else:
+    print("false")'''
+"""solution 2""""""a=int(input())
+if a>18:
+    print("above 18")
+if a<18:
+    print("less 18")
+if a==18:
+    print("exact 18")"""
+"""solution 3"""'''a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")'''
+"""solution 4""" 
+"""a=int(input())
+if a>18:print("eligible to vote")
+else:(print("inelgible to vote"))"""
+"""solution 5"""
+'''a=int(input("marks="))
+if a<=20:print("pass")
+elif a<=40:print("average")
+elif a<=80:print("good")
+else:print("fail")'''
+'''solution 6'''
+"""a=int(input())
+if a<10:print("very cold")
+elif a>=10 and a<=19:print("cold")
+elif a>=20 and a<=29:print("normal")
+elif a>=30: print("hot")"""
+"""solution 7"""
+'''a=int(input())
+if a>=18:print("adult")
+if a>21:print("Above 21")
+if a>60:print("Senior Citizen")'''
+"""Solution 8"""
+"""a=int(input())
+if a>0:print("positive")
+if a>10:print("Greater than 10")
+if a%2==0:print("even")"""
+"""a=int(input())
+if a>0:print("positive")
+if a==0:print("equal")
+if a<0:print("less")"""
+'''a=int(input())
+if a%2==0:print("even")
+else:print("odd")'''
+"""a=int(input())
+b=int(input())
+c=int(input())
+if a>b:print("a big")
+if b>a:print("b big")
+if c>b:print("c big")"""
+a=int(input())
+if a>=18:
+    if a>=60:print("old") 
+    else: print("adult") 
+else:print("minor")
